40b69c388d31d2314bbf2a56ff97cd3384a94bc8
Eduard Ramirez <me@tantaroth.com>
2022-01-30 13:38:47 -0500

    add angular fire

--- projects/main/src/environments/environment.prod.ts
+++ projects/main/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'navi-fast',
+    appId: '1:2072652224:web:9f09d2ff27231869f20025',
+    storageBucket: 'navi-fast.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAiFKZqV2zAyY-UIYK97NykSUHSgfE5Lxk',
+    authDomain: 'navi-fast.firebaseapp.com',
+    messagingSenderId: '2072652224',
+  },
   production: true
 };
--- projects/main/src/environments/environment.ts
+++ projects/main/src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'navi-fast',
+    appId: '1:2072652224:web:9f09d2ff27231869f20025',
+    storageBucket: 'navi-fast.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAiFKZqV2zAyY-UIYK97NykSUHSgfE5Lxk',
+    authDomain: 'navi-fast.firebaseapp.com',
+    messagingSenderId: '2072652224',
+  },
   production: false
 };
 
